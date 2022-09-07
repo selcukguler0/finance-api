f42e8952699bea1fceef89098c04f4fdba2bd7b9
selçuk güler <2003022022@student.beykent.edu.tr>
2022-09-07 08:45:56 +0300

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as Cors from "cors"
+import corsMiddleware from "../../utils/corsMiddleware"
 
-export default function handler(req, res) {
+// Initializing the cors middleware
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+})
+
+export default async function handler(req, res) {
+
   res.status(200).json({ name: 'John Doe' })
 }
