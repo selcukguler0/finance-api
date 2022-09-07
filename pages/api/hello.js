03ce446e1c84198f48599edd54bbc8e4da34c4e4
selçuk güler <2003022022@student.beykent.edu.tr>
2022-09-07 21:27:17 +0300

    hello edit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,16 +1,4 @@
-import * as Cors from "cors"
-import corsMiddleware from "../../utils/corsMiddleware"
-
-import axios from "axios";
-import jsdom from "jsdom";
-
-// Initializing the cors middleware
-const cors = Cors({
-	methods: ['GET', 'HEAD'],
-})
-
 export default async function handler(req, res) {
-	await corsMiddleware(req, res, cors);
 	const url = "https://www.google.com/search?q=hello+world";
 	const response = await axios.get(url);
 	const html = response.data;
