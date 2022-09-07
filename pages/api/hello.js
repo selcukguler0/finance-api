c1b2e1849a2336a985d661fc2e1d7f089385ced4
selçuk güler <2003022022@student.beykent.edu.tr>
2022-09-07 08:52:08 +0300

    without cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,20 @@
 import * as Cors from "cors"
 import corsMiddleware from "../../utils/corsMiddleware"
 
+import axios from "axios";
+import jsdom from "jsdom";
+
 // Initializing the cors middleware
 const cors = Cors({
 	methods: ['GET', 'HEAD'],
 })
 
 export default async function handler(req, res) {
-
-  res.status(200).json({ name: 'John Doe' })
+	// await corsMiddleware(req, res, cors);
+	const url = "https://www.google.com/search?q=hello+world";
+	const response = await axios.get(url);
+	const html = response.data;
+	const dom = new jsdom.JSDOM(html);
+	console.log(dom.window.document.querySelector("title").textContent);
+	res.status(200).json({ name: dom.window.document.querySelector("title").textContent })
 }
